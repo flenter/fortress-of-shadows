a44d7f33876a74dfe6db9331f19777ad499baeed
Jacco Flenter <flenter@users.noreply.github.com>
2023-08-24 16:39:09 +0200

    add enemy types

--- src/Enemy.ts
+++ src/Enemy.ts
@@ -5,6 +5,63 @@ import { VisualElement } from "./VisualElement";
 
 let id = 0;
 
+function getEnemy() {
+  const stats = [
+    {
+      name: "big_demon_run_anim_f",
+      speed: 0.2,
+    },
+    {
+      name: "angel_run_anim_f",
+      speed: 1.5,
+    },
+    {
+      name: "big_zombie_run_anim_f",
+      speed: 0.25,
+    },
+    // "dwarf_f_run_anim_f",
+    {
+      name: "elf_m_run_anim_f",
+      speed: 1.1,
+    },
+    {
+      name: "slug_anim_f",
+      speed: 0.1,
+    },
+    {
+      name: "goblin_run_anim_f",
+      speed: 1,
+    },
+    {
+      name: "knight_m_run_anim_f",
+      speed: 0.9,
+    },
+    {
+      name: "lizard_f_run_anim_f",
+      speed: 1.2,
+    },
+    // "masked_orc_run_anim_f",
+    {
+      name: "ogre_run_anim_f",
+      speed: 1,
+    },
+    // "orc_shaman_run_anim_f",
+    // "orc_warrior_run_anim_f",
+    // "pumpkin_dude_run_anim_f",
+    // "skelet_run_anim_f",
+    // "tiny_zombie_run_anim_f",
+    {
+      name: "wizzard_f_run_anim_f",
+      speed: 1.05,
+    },
+    // "wogol_run_anim_f",
+    // "chort_run_anim_f",
+    // "doc_run_anim_f",
+  ];
+  const index = Math.floor(Math.random() * stats.length);
+  return stats[index];
+}
+
 export class Enemy implements VisualElement {
   id: number;
   previousCoordinates: Coordinates | undefined;
@@ -14,6 +71,7 @@ export class Enemy implements VisualElement {
   sprite: VisualElement["sprite"];
   text: PIXI.Text;
   speed = 1;
+  type: string;
 
   constructor(coordinates: Coordinates, direction: Direction) {
     this.id = id++;
@@ -22,25 +80,29 @@ export class Enemy implements VisualElement {
     this.direction = direction;
     this.text = new PIXI.Text(String(this.id));
     this.sprite = new PIXI.Container();
+    const stats = getEnemy();
+    this.type = stats.name;
+    this.speed = stats.speed;
     this.initSprite();
   }
 
   initSprite() {
     const { data } = PIXI.Assets.cache.get("/assets/assets.json");
     const { animations } = data;
-    const monster = PIXI.AnimatedSprite.fromFrames(
-      animations["big_demon_run_anim_f"],
-    );
+    const animation = animations[this.type];
+    if (!animation) {
+      debugger;
+    }
+
+    const monster = PIXI.AnimatedSprite.fromFrames(animation);
     this.sprite.addChild(monster);
-    this.sprite.addChild(this.text);
+    // this.sprite.addChild(this.text);
     monster.play();
-    monster.animationSpeed = 0.2;
+    monster.animationSpeed = 0.15;
     const { x, y } = this.translateToScreenCoordinates(this.coordinates);
     this.sprite.x = x;
     this.sprite.y = y;
     this.sprite.zIndex = this.coordinates.y;
-    this.text.y = -16;
-
   }
 
   translateToScreenCoordinates(coordinates: Coordinates): Coordinates {
@@ -49,6 +111,7 @@ export class Enemy implements VisualElement {
       y:
         coordinates.y * TILE_SIZE +
         0.5 * TILE_SIZE -
+        this.sprite.height +
         0.5 * this.sprite.height -
         8,
     };
